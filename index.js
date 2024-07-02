8fadaf569aca285e4281044bddf444df6a997c4d
Fawaz Oduola <oduolafawaz@gmail.com>
2024-07-02 14:18:21 +0100

    fixed ip

--- index.js
+++ index.js
@@ -11,39 +11,43 @@ dotenv.config();
 const ipinfoWrapper = new IPinfoWrapper(process.env.MY_TOKEN);
 
 app.get("/api/hello", async (req, res) => {
-  const { visitor_name } = req.query;
-  const ipdata = request.headers["x-forwarded-for"] || req.ip;
+  try {
+    const { visitor_name } = req.query;
+    const ipdata = request.headers["x-forwarded-for"] || req.ip;
 
-  let location_res = await ipinfoWrapper.lookupIp(ipdata);
-  if (!visitor_name) {
-    return res.json({
-      error: "visitor name not available",
-    });
-  }
+    let location_res = await ipinfoWrapper.lookupIp(ipdata);
+    if (!visitor_name) {
+      return res.json({
+        error: "visitor name not available",
+      });
+    }
 
-  let weather_res = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${location_res.city}&aqi=no`
-  );
-  const weather_data = await weather_res.json();
-
-  if (weather_data.error) {
-    location_res = await ipinfoWrapper.lookupIp([]);
-    weather_res = await fetch(
+    let weather_res = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${location_res.city}&aqi=no`
     );
-    const response = {
-      client_ip: req.ip,
-      location: location_res.city,
-      greetings: `Hello ${visitor_name}!, the temperature is ${weather_data?.current?.temp_c} degrees Celcius in ${location_res?.city}`,
-    };
-    return res.json(response);
-  } else {
-    const response = {
-      client_ip: req.ip,
-      location: location_res.city,
-      greetings: `Hello ${visitor_name}!, the temperature is ${weather_data.current.temp_c} degrees Celcius in ${location_res.city}`,
-    };
-    return res.json(response);
+    const weather_data = await weather_res.json();
+
+    if (weather_data.error) {
+      location_res = await ipinfoWrapper.lookupIp([]);
+      weather_res = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=${location_res.city}&aqi=no`
+      );
+      const response = {
+        client_ip: req.ip,
+        location: location_res.city,
+        greetings: `Hello ${visitor_name}!, the temperature is ${weather_data?.current?.temp_c} degrees Celcius in ${location_res?.city}`,
+      };
+      return res.json(response);
+    } else {
+      const response = {
+        client_ip: req.ip,
+        location: location_res.city,
+        greetings: `Hello ${visitor_name}!, the temperature is ${weather_data.current.temp_c} degrees Celcius in ${location_res.city}`,
+      };
+      return res.json(response);
+    }
+  } catch (error) {
+    return res.json({ errorCode: 500, error });
   }
 });
 
