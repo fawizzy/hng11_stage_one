b9004a613eb5210dd93429b13d082cf7adbe5fc4
Fawaz Oduola <oduolafawaz@gmail.com>
2024-07-02 12:39:17 +0100

    fix

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
 
 const express = require("express");
-const IPinfoWrapper = require("node-ipinfo").IPinfoWrapper;
-//import IPinfoWrapper, { IPinfo, AsnResponse } from "node-ipinfo";
-
+const { IPinfoWrapper } = require("node-ipinfo");
 const app = express();
+const port = 3000;
+dotenv.config();
+
 const ipinfoWrapper = new IPinfoWrapper(process.env.MY_TOKEN);
 
-const bodyParser = require("body-parser");
-const path = require("path");
-
-app.use(express.static("public"));
-
-app.get("/", function (req, res) {
-  res.send("hello world");
-});
-
-app.get("/hello", async (req, res) => {
+app.get("/api/hello", async (req, res) => {
   const { visitor_name } = req.query;
   let location_res = await ipinfoWrapper.lookupIp(req.ip);
   const weather_res = await fetch(
@@ -25,21 +17,25 @@ app.get("/hello", async (req, res) => {
   const weather_data = await weather_res.json();
   //let location_data = await location_res.json();
 
+  if (!visitor_name) {
+    return res.json({
+      error: "visitor name not available",
+    });
+  }
   if (location_res.bogon) {
-    res.json({
+    return res.json({
       error: "your location cannot be found",
     });
-    res.end();
   } else {
     const response = {
       client_ip: req.ip,
       location: location_res.city,
       greetings: `Hello ${visitor_name}!, the temperature is ${weather_data.current.temp_c} degrees Celcius in ${location_res.city}`,
     };
-    res.send(response);
+    return res.json(response);
   }
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
